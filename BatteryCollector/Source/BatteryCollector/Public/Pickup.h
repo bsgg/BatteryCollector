3461f70dc11c5eb4e00741f5452bee0d6117932b
Mart-PC\Mart <beatriz.s.g86@gmail.com>
2017-07-04 09:00:18 +0100

    Pickup class added

---
+++ BatteryCollector/Source/BatteryCollector/Public/Pickup.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Pickup.generated.h"
+
+UCLASS()
+class BATTERYCOLLECTOR_API APickup : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APickup();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Return the mesh for the pickup
+	FORCEINLINE class UStaticMeshComponent* GetMesh() const { return PickupMesh; };
+
+protected:
+	// Static mesh to represent the pickup in the level
+	// BlueprintReadOnly: It can't remove it
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pickup", meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* PickupMesh;
+	
+};
